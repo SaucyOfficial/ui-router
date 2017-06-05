83ef1b085e77df7cb1ac30b457d929c906ca2dcb
nicholas maestrello Agiz <rainickmr@gmail.com>
2017-06-05 14:23:21 -0300

    Add files via upload

---
+++ helloworld.js
@@ -0,0 +1,39 @@
+var myApp = angular.module('helloworld', ['ui.router']);
+
+myApp.config(function($stateProvider) {
+  var helloState = {
+    name: 'hello',
+    url: '/hello',
+    template: '<h3>hello world!</h3>'
+  }
+
+  var aboutState = {
+    name: 'about',
+    url: '/about',
+    template: '<h3>Its the UI-Router hello world app!</h3>'
+  }
+
+  var contentState={
+    name:'content',
+    url:'/content',
+    template:'<h1>This is the content of site</h1>'
+  }
+
+  var homeState={
+    name:'home',
+    url:'/home',
+    templateUrl:'views/home.html'
+  }
+
+  var formState={
+    name:'form',
+    url:'/form',
+    templateUrl:'views/form.html'
+  }
+
+  $stateProvider.state(homeState);
+  $stateProvider.state(formState);
+  $stateProvider.state(contentState);
+  $stateProvider.state(helloState);
+  $stateProvider.state(aboutState);
+});
